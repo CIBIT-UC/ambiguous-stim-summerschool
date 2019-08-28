320cff86d34a8dbd56a6cf1fb9867696a1951261
Bruno Miguel <migueldireito@gmail.com>
2019-08-28 17:29:55 +0100

    Update plotting_data.m

--- data-analysis/plotting_data.m
+++ data-analysis/plotting_data.m
@@ -1,6 +1,12 @@
 %%
 mean_ERP= squeeze(mean(data_cond11_run, 1))';
+mean_ERP_allchan_even= squeeze(mean(mean_ERP(1:2:40), 1));
+mean_ERP_allchan_odd= squeeze(mean(mean_ERP(2:2:40), 1));
+
+
 colors_ERP_chan=summer(40); 
+
+
 
 %%
 figure, 
@@ -11,3 +17,11 @@ for i=1:40
     pause(.5)
     
 end
+
+
+%%
+
+figure, 
+plot(mean_ERP_allchan_even, 'r')
+hold on
+plot(mean_ERP_allchan_odd, 'b')
